5b5af3527447666fb3836340625b0877281974dc
Usuario <Usuario@DESKTOP-P4Q281R>
2021-11-26 08:21:51 -0500

    Add project files.

---
+++ CarritoCompras/CarritoCompras/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using AppNotas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarritoCompras
+{
+    public class AppSettings
+    {
+        public static string ApiFirebase = "https://ventas01-167c9-default-rtdb.firebaseio.com/";
+        private static string KeyAplication = "AIzaSyCnoYBOuIn4yJVrDpHHHMIq3i3GHxP3kyI";
+
+
+        public static ResponseAuthentication oAuthentication { get; set; }
+        private static string ApiUrlGoogleApis = "https://identitytoolkit.googleapis.com/v1/";
+
+        public static string ApiAuthentication(string tipo)
+        {
+            if (tipo == "LOGIN")
+                return ApiUrlGoogleApis + "accounts:signInWithPassword?key=" + KeyAplication;
+            else if (tipo == "SIGNIN")
+                return ApiUrlGoogleApis + "accounts:signUp?key=" + KeyAplication;
+            else
+                return "";
+        }
+
+    }
+}
